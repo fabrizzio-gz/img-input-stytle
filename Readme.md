f05caceeae1ea08990648d4f3a5b346ba6721b86
fabrizzio-gz <fabrizzio.gz@gmail.com>
2022-01-07 00:40:47 -0400

    First commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Styled image input
+
+Cuustom HTML input with a "+" button to add images.
+
+![custom image input preview](https://onestepcode.com/wp-content/uploads/2022/01/add-img-button.png)
+
+See [blog post](https://onestepcode.com/style-html-img-file-input) for demo and explanation.
---
+++ scripts.js
@@ -0,0 +1,64 @@
+// Global variables
+const imgInputHelper = document.getElementById("add-single-img");
+const imgInputHelperLabel = document.getElementById("add-img-label");
+const imgContainer = document.querySelector(".custom__image-container");
+const imgFiles = [];
+
+const addImgHandler = () => {
+  const file = imgInputHelper.files[0];
+  if (!file) return;
+
+  // Generate img preview
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => {
+    const newImg = document.createElement("img");
+    newImg.src = reader.result;
+    imgContainer.insertBefore(newImg, imgInputHelperLabel);
+  };
+
+  // Store img file
+  imgFiles.push(file);
+
+  // Reset image input
+  imgInputHelper.value = "";
+  return;
+};
+
+const getImgFileList = (imgFiles) => {
+  const imgFilesHelper = new DataTransfer();
+  imgFiles.forEach((imgFile) => imgFilesHelper.items.add(imgFile));
+  return imgFilesHelper.files;
+};
+
+const printFiles = (files, container) => {
+  container.innerHTML = "";
+  for (let i = 0; i < files.length; i++) {
+    const p = document.createElement("p");
+    p.textContent = `File: ${files[i].name}, size: ${files[i].size}`;
+    container.appendChild(p);
+  }
+};
+
+const basicFormSubmitHandler = (ev) => {
+  ev.preventDefault();
+  const files = document.getElementById("basic-image-input").files;
+  const container = document.getElementById("basic__print-files");
+  printFiles(files, container);
+};
+
+const customFormSubmitHandler = (ev) => {
+  ev.preventDefault();
+  const firstImgInput = document.getElementById("image-input");
+  firstImgInput.files = getImgFileList(imgFiles);
+  const container = document.getElementById("custom__print-files");
+  printFiles(firstImgInput.files, container);
+};
+
+imgInputHelper.addEventListener("change", addImgHandler);
+document
+  .querySelector(".custom__form")
+  .addEventListener("submit", customFormSubmitHandler);
+document
+  .querySelector(".basic__form")
+  .addEventListener("submit", basicFormSubmitHandler);
